850e135ef540d7264da11358685ab5bd215b6b9e
Muhammad Nofil Shoaib <164288852+MuhammadNofilShoaib@users.noreply.github.com>
2024-10-21 16:02:56 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,14 @@
+const div = document.getElementById('main')
+let display = 0;
+ function hideshow () {
+     if (display == 1)
+         {
+            div.style.display = 'block'
+            display = 0;
+         }
+
+         else {
+            div.style.display = 'none';
+            display = 1;
+         }
+ }
